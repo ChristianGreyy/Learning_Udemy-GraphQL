bbf1a98f1d8b88e70ca5434825cd4ad5aea1116c
Christian Grey <truongthanhhung2k2@gmail.com>
2022-02-28 23:36:47 +0700

    Update

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,23 @@
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
 
 module.exports = {
     createUser: async function({ userInput }, req) {    
+        const errors = [];
+        if(!validator.isEmail(userInput.email)) {
+            errors.push({ message: 'E-Mail is invalid.' });
+        }
+        if(validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+            errors.push({ message: 'Password too short!' })
+        }
+        if(errors.length > 0) {
+            const error = new Error('Invalid Input.');
+            error.data = errors;
+            error.code = 422;
+            throw error;
+        }
         const existingUser = await User.findOne({email: userInput.email})
         if(existingUser) {
             const error = new Error('User exists already');
@@ -18,5 +33,32 @@ module.exports = {
         const createUser = await user.save();
 
         return {...createUser.toJSON(),_id: createUser._id.toString()}
+    },
+    async login({ email, password }) {
+        const user = await User.findOne({ email: email });
+
+        if(!user) {
+            const error = new Error('User not found');
+            error.code = 401;
+            throw error;
+        }
+
+        const isEqual = await bcrypt.compare(password, user.password);
+        if(!isEqual) {
+            const error = new Error('Password is incorrect');
+            error.code = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            userId: user._id.toString(),
+            email: user.email,
+        }, 'somesupersecretsecret', {
+            expiresIn: '1h'
+        });
+
+        return {
+            token,
+            userId: user._id.toString(),
+        }
     }
 }
